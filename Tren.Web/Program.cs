c61dcaf3cef854721145ddceae4f9c3c03320c8a
Cameron Harris <cameron@cameronharris.org>
2021-07-03 15:10:15 +0100

    Initial commit

---
+++ Tren.Web/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Tren.Web
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
